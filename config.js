4868b85bd67abfdd9a4c31ad250501267e2d3eae
biscuitboy1980 <biscuitboy1980@gmail.com>
2019-09-01 17:57:53 -0700

    Signed-off-by: biscuitboy1980 <biscuitboy1980@gmail.com>

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-module.exports.token = "NjE2NzgxNjI4ODAyMzM0NzM1.XWxnaw.SdZlS1aW_iGKzoIl_xb2AXPbuJE";
+module.exports.token = "NjE2NzgxNjI4ODAyMzM0NzM1.XWxo5w.anE4_O5gTePLEe6GpMVsZpbRo_s";
 
 module.exports.prefix = "&";
